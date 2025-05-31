50b30ce80ab36bbf610ccbc04d24a43598596bc9
welbertim <welbertimoreira@hotmail.com>
2025-05-31 17:46:09 -0300

    Atualiza kotlin + Refatoracoes de arquitetura

---
+++ app/src/main/java/com/picpay/desafio/android/presentation/extensions/ViewExtensions.kt
@@ -0,0 +1,11 @@
+package com.picpay.desafio.android.presentation.extensions
+
+import android.view.View
+
+/**
+ * Created by Welbert on 31/05/2025
+ */
+
+
+fun View.show() { visibility = View.VISIBLE }
+fun View.hide() { visibility = View.GONE }
